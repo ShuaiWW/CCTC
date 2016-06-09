30ceb64b580dc20d8e17d7d7d84ba7dc9b8091a3
ShuaiWW <wang6839@umn.edu>
2016-06-09 17:01:22 -0500

    Add files via upload

--- cctc.py
+++ cctc.py
@@ -4,44 +4,101 @@ import matplotlib.pyplot as plt
 prime=[13,17,19,23,29,31,37,41,43,47,53,59,61]
 
 
-
-def getData(Folds=[17],repeate=3,y=[1]):
+def getData(Folds=[17],repeate=3,signal=[1]):
 	Data={}
 	temp=[]
 	error=0
 	res=[]
+	rr=0
+	useful_energy={}
+	y=[]
+
+	for item in signal:
+		if item>0.01:
+			y.append(1)
+		else:
+			y.append(0)
 	for multi in Folds:
 		Data[multi]={}
+		useful_energy[multi]=[]
+		#start folding
+		start=0
 
-		for j in range(len(y)/repeate):
-			a=np.zeros(multi)
-			for i in range(repeate):
-				for ii in range(multi):
-					temp.append(y[ii+i*multi])
-				a+=np.array(temp)
+		while start<len(y)-repeate*multi:
+			#start folding:
+			
+			folding=np.zeros(multi)
+			temp=[]
+			signal_index=[]
+			for ii in range(repeate):
+				for i in range(multi):
+					useful_energy[multi].append(0)
+					signal_index.append(len(temp))
+					temp.append(y[start+ii*multi+i])
+
+				# compute folding sum
+				folding+=np.array(temp)
 				temp=[]
+			#find out CTC data
 
-			data_temp=[]
-			for i in range(multi):
-				res.append(a[i])
-				#if a[i]==(repeate/2+1):
-				if a[i]==repeate:
-					data_temp.append(i)
-			if len(data_temp)>=2:
-				d=data_temp[1]-data_temp[0]
+			fold_temp=[]
+			for index in range(multi):
+				ff.append(folding[index])
+				if folding[index]==repeate:
+
+					#mark time slots whose sum is equal to repeate
+					fold_temp.append(index)
+
+					# get energy values on these time slots
+					for might_index in range(len(signal_index)):
+						if signal_index[might_index]==index:
+							useful_energy[multi][start+might_index]=signal[start+might_index]			
+
+			if len(fold_temp)>=2:
+
+				# compute CTC data
+				d=fold_temp[1]-fold_temp[0]
+
 				if d<multi/2:
 					if Data[multi].get(d)==None:
-						Data[multi]={d:1}
+						Data[multi][d]=1
 					else:
 						Data[multi][d]+=1
 				else:
+
 					if Data[multi].get(multi-d)==None:
-						Data[multi]={multi-d:0}
+						Data[multi][multi-d]=1
 					else:
 						Data[multi][multi-d]+=1
 			else:
 				error+=1
-	return Data,res,error
+			start+=repeate*multi
+
+	return Data,useful_energy,error
+
+
+def getChannel(interval,signal):
+	# interval is a dictionary consisting of interval set of 4 ZigBee channels
+	# signal is received energy
+	for channel_index in range(len(interval)):
+		data,res,error=getData(intervals,3,signal)
+
+def equation(signal):
+
+# 1st phase: get exact energy information on each multiplex ZigBee channel
+
+# 2nd phase: substitute these energy values into equations and get precise energy information on all ZigBee channels
+
+
+def cctc(signal):
+# return cross-technology communication data extracted from energy information received by Wi-Fi device
+
+# 1st phase, solve equations and get energy information in each ZigBee chanel
+
+# 2nd phase, Demodulation on each Zigbee channels and get CTC data
+
+
+
 
 def generate_multiplex(intervals,howlong=5,num=1,data=3):
 	import numpy as np
@@ -50,16 +107,17 @@ def generate_multiplex(intervals,howlong=5,num=1,data=3):
 	temp=np.zeros(1000*howlong)
 	for i in range(num):
 		start=np.random.randint(11);
+		signal_energy=np.random.random_sample()*0.01
 		for j in range(1000*howlong):
 			if j>=start:
 				if (j-start)%intervals[i]==0:
-					temp[j]=np.random.random_sample()*0.01+0.01;
+					temp[j]=signal_energy+0.01;
 				if (j-start-data)%intervals[i]==0:
-					temp[j]=np.random.random_sample()*0.01+0.01;
+					temp[j]=signal_energy+0.01;
 	return list(temp)
 
 
-def generate_packets(user_num=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,],packet_len=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,],how_long=5):
+def generate_packets(user_num=[1,1,1,1,1,1,1,1,1,1,1,1,1,1],packet_len=[1,1,1,1,1,1,1,1,1,1,1,1,1,1],how_long=5):
 	# user_num is number of users on each ZigBee channel
 	# packet_len is the length of each Zigbee channel
 	# how_long is how long time this simulation runs
@@ -77,13 +135,14 @@ def generate_packets(user_num=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,],packet_len=[1,1,1,1
 		num=user_num[channel]
 		temp=np.zeros(1000*how_long)
 		for i in range(num):
+			signal_energy=np.random.random_sample()*0.01
 			start=np.random.randint(11);
 			for j in range(1000*how_long):
 				if j>=start:
 					if (j-start)%intervals[i]==0:
-						temp[j]=np.random.random_sample()*0.01+0.01;
+						temp[j]=signal_energy+0.01;
 					if (j-start-data[i])%intervals[i]==0:
-						temp[j]=np.random.random_sample()*0.01+0.01;
+						temp[j]=signal_energy+0.01;
 		RSSI_channel.append(list(temp))
 
 	RSSI_channel.append(generate_multiplex(intervals=interval1,num=user_num[11],howlong=how_long,data=data[11]))
@@ -115,20 +174,19 @@ def wifi(ZigBee):
 	return Wi
 
 
-ZigBee=generate_packets()
+ZigBee=generate_packets(user_num=[2,2,2,2,2,2,2,2,2,2,2,2,2,2],packet_len=[1,1,1,1,1,1,1,1,1,1,1,1,1,1],how_long=5)
+
+
+ZigBee=generate_packets(user_num=[1,1,1,1,1,1,1,1,1,1,1,1,1,1],packet_len=[1,1,1,1,1,1,1,1,1,1,1,1,1,1],how_long=5)
 wi=wifi(ZigBee)
 multiplex=np.matrix(wi)
 temp=multiplex.transpose()
 multiplex=temp.tolist()[10]
 
+intervals=[11,13,61,67,71,73,79,83];
+interval1=[17,31,47,89,109,101,103,107]
+interval2=[19,37,53,97,139,127,131,137]
+interval3=[23,43,59,113,149,151,157,163]
+interval={1:intervals,2:interval1,3:interval2,4:interval3}
+Data,res,error=getData(intervals,3,multiplex)
 
-y=[]
-
-for item in multiplex:
-	if item>0.01:
-		y.append(1)
-	else:
-		y.append(0)
-
-
-Data,res,error=getData([17,19,23],3,y)
